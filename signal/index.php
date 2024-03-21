0eb9fbfce10a44cad73081c2f9b166d9cc39f943
pentair <en.gannim@gmail.com>
2024-03-21 02:27:45 +0000

    Remove sqlite files

--- signal/index.php
+++ signal/index.php
@@ -1,5 +1,6 @@
 <?php
 
+die();
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -8,8 +9,8 @@ $signal = file_get_contents('php://input');
 
 $signalDb = new SQLite3('/var/www/pentair/data/signal.sql3');
 $signalDb->exec("CREATE TABLE IF NOT EXISTS signals (id INTEGER PRIMARY KEY AUTOINCREMENT, ts DATETIME DEFAULT CURRENT_TIMESTAMP, signal TEXT)");
-$signalDb->exec("select * from signals where date(ts) < date('now', '-6 day')");
-$signalDb->exec("VACUUM;");
+//$signalDb->exec("select * from signals where date(ts) < date('now', '-6 day')");
+//$signalDb->exec("VACUUM;");
 
 $stmt = $signalDb->prepare("INSERT INTO signals (signal) values (:signal)");
 $stmt->bindParam('signal', $signal);
